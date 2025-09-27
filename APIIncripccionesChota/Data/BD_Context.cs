2ccd6e5793fdf90b7d8124c2c8861b43416aaeef
EberTasillaAguilar <etasilla22_2@unc.edu.pe>
2025-09-26 23:23:40 -0500

    Primera vez

---
+++ APIIncripccionesChota/Data/BD_Context.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace APIIncripccionesChota.Data
+
+{
+    public class BD_Context: DbContext
+    {
+        public BD_Context(DbContextOptions<BD_Context> options): base(options) { 
+        }
+        // Definición de las tablas
+        public DbSet<Pago> Pagos { get; set; }
+        public DbSet<Pre> PregradoCertificados { get; set; }
+        public DbSet<Pago> CentroIdiomas { get; set; }
+        public DbSet<Pago> PregradoTasas { get; set; }
+        public DbSet<Pago> Cepunc { get; set; }
+
+        // Configuración del modelo, opcionalmente se puede hacer con Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuración para la tabla de 'Pagos'
+            modelBuilder.Entity<Pago>()
+                .ToTable("Pagos");
+
+            // Configuración de los otros tipos de pagos si es necesario
+            modelBuilder.Entity<Pago>()
+                .ToTable("PregradoCertificados");
+
+            modelBuilder.Entity<Pago>()
+                .ToTable("CentroIdiomas");
+
+            modelBuilder.Entity<Pago>()
+                .ToTable("PregradoTasas");
+
+            modelBuilder.Entity<Pago>()
+                .ToTable("Cepunc");
+
+            // Si deseas relaciones más complejas, podrías hacer algo similar
+            // ejemplo, si se tuvieran otras relaciones entre las tablas:
+            // modelBuilder.Entity<Pago>()
+            //    .HasOne(o => o.AlgunaOtraEntidad)
+            //    .WithMany()
+            //    .HasForeignKey(f => f.AlgunaClaveForanea);
+        }
+    }
+}
